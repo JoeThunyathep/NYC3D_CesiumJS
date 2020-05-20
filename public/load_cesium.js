df20a24582520dd856e56e99ae44e3ff43ab3269
Joe <thunyathep.s@outlook.com>
2020-05-20 16:41:58 +0200

    update

--- public/load_cesium.js
+++ public/load_cesium.js
@@ -1,6 +1,8 @@
 var viewer = new Cesium.Viewer('cesiumContainer');
 var tileset = viewer.scene.primitives.add(new Cesium.Cesium3DTileset({
-    url: "../3dtile_nyc/tileset.json",
+    // url: "../3dtile_nyc/tileset.json",
+    // url: "http://193.196.37.89:8092/AssetsHFT/3DTile_Niedernhalle/tileset.json",
+    url: "http://steinbeis-3dps.eu:8080/3DContainerTile/collections/NewYork/3DTiles/",
 }))
 Cesium.when(tileset.readyPromise).then(function (tileset) {viewer.flyTo(tileset)})
 // chroma.scale(['#fafa6e','#2A4858']).mode('lch').colors(6)
